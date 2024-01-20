26abeae0cdc96ecba0dc91920a9056e80da65fc7
javabykondal <pavuluriyedukondalu98@gmail.com>
2024-01-20 17:14:44 +0530

    first commit

---
+++ src/main/java/org/example/OldStudent.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import javax.persistence.*;
+
+@Entity
+@DiscriminatorValue("OSTU")
+public class OldStudent extends Student {
+    @Column(name = "OldId")
+
+    private int oid;
+    @Column(name = "Oname")
+    private String oname;
+    @Column(name = "OEmail")
+    private String oemail;
+
+    public OldStudent(int oid, String oname, String oemail) {
+        this.oid = oid;
+        this.oname = oname;
+        this.oemail = oemail;
+    }
+
+    public OldStudent(String roll, String name, int oid, String oname, String oemail) {
+        super(roll, name);
+        this.oid = oid;
+        this.oname = oname;
+        this.oemail = oemail;
+    }
+
+    public OldStudent(int id, String roll, String name, int oid, String oname, String oemail) {
+        super(id, roll, name);
+        this.oid = oid;
+        this.oname = oname;
+        this.oemail = oemail;
+    }
+
+    public int getOid() {
+        return oid;
+    }
+
+    public void setOid(int oid) {
+        this.oid = oid;
+    }
+
+    public String getOname() {
+        return oname;
+    }
+
+    public void setOname(String oname) {
+        this.oname = oname;
+    }
+
+    public String getOemail() {
+        return oemail;
+    }
+
+    public void setOemail(String oemail) {
+        this.oemail = oemail;
+    }
+
+    @Override
+    public String toString() {
+        return "OldStudent{" +
+                "oid=" + oid +
+                ", oname='" + oname + '\'' +
+                ", oemail='" + oemail + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/WeklyStudent.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import javax.persistence.*;
+
+@Entity
+@DiscriminatorValue("wekSU")
+public class WeklyStudent  extends CurrentStudent{
+    @Column(name = "WeekNAmes")
+    private String weekName;
+    @Column(name = "weekItems")
+    private  String WeekTime;
+
+
+    public WeklyStudent(int feebal, String trainer, String time, String weekName, String weekTime) {
+        super(feebal, trainer, time);
+        this.weekName = weekName;
+        WeekTime = weekTime;
+    }
+
+    public WeklyStudent(String roll, String name, int feebal, String trainer, String time, String weekName, String weekTime) {
+        super(roll, name, feebal, trainer, time);
+        this.weekName = weekName;
+        WeekTime = weekTime;
+    }
+
+    public WeklyStudent(int id, String roll, String name, int feebal, String trainer, String time, String weekName, String weekTime) {
+        super(id, roll, name, feebal, trainer, time);
+        this.weekName = weekName;
+        WeekTime = weekTime;
+    }
+
+    public String getWeekName() {
+        return weekName;
+    }
+
+    public void setWeekName(String weekName) {
+        this.weekName = weekName;
+    }
+
+    public String getWeekTime() {
+        return WeekTime;
+    }
+
+    public void setWeekTime(String weekTime) {
+        WeekTime = weekTime;
+    }
+
+    @Override
+    public String toString() {
+        return "WeklyStudent{" +
+                "weekName='" + weekName + '\'' +
+                ", WeekTime='" + WeekTime + '\'' +
+                '}';
+    }
+}
